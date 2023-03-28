3962c0460e17feac5bfe7284ae0de34f15b474c6
NAbersons <119396993+NAbersons@users.noreply.github.com>
2023-03-28 08:47:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
 
-console.log(plusins)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
+
+console.log(Add(2,3))
+console.log(Subtract(2,3))
+console.log(Multiply(2,3))
+console.log(Divide(2,3))
